78b5c4edb2c2360bed2c3f54179b0f4a87678c22
kentFrontEndDev <duccuong.hpe@gmail.com>
2020-12-19 15:06:40 +0700

    use helmet to protect http

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const app = express();
 const upload = multer();
@@ -34,17 +35,20 @@ const usersRoute = require("./routers/users.route");
 const { gobalErrorHandle } = require("./controllers/error.controler");
 const { requireSignin } = require("./controllers/auth.controler");
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(express.static(`${__dirname}/public`));
+
+app.use(helmet()); // helmet help you secure express app by setting various http header
 
 if (process.env.NODE_ENV === "develope") {
   app.use(morgan("dev"));
 }
 
 app.use(getRequestTime);
+
 app.use(upload.none());
 
-app.use("/api/v1", appLimit);
+app.use("/api", appLimit); // limit request from same ip
 
 app.use("/api/v1/tours", requireSignin, toursRoute);
 app.use("/api/v1/users", usersRoute);
